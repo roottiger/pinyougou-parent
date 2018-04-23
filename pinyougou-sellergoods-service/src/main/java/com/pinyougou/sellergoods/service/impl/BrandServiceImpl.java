dfbdcad1fff33461adfdebe14fc289ab659f7156
roottiger <469478362@qq.com>
2018-04-23 11:30:06 +0800

    搭好环境

---
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+@Service//使用dubbo的注解
+public class BrandServiceImpl implements BrandService {
+    @Autowired
+    private TbBrandMapper brandMapper;
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+    }
+}
